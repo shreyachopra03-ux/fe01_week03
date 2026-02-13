b4c4af00d61653c208d7481aa53a89d900acea6f
Shreya Chopra <chopra.shreya03@gmail.com>
2026-02-14 02:29:37 +0530

    Update: Added the slider functionality

--- VerticalSliderProject/index.js
+++ VerticalSliderProject/index.js
@@ -3,12 +3,12 @@ const slideLeft = document.querySelector('.left-slide');
 const slideRight = document.querySelector('.right-slide');
 const buttonDown = document.querySelector('.down-button');
 const buttonUp = document.querySelector('.up-button');
-const slidesLength = slideRight.querySelectorAll('.div');
+const slidesLength = slideRight.querySelectorAll('div').length;
 
-let activeSlidesLength = 0;
+let activeSlidesIndex = 0;
 
 // This top css property tells how much the element has to be shifted from top
-slideLeft.style.top = `${(slidesLength - 1) * 100}vh`;
+slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`;
 
 buttonUp.addEventListener('click', () => changeSlide('up'));
 buttonDown.addEventListener('click', () => changeSlide('down'));
@@ -16,21 +16,19 @@ buttonDown.addEventListener('click', () => changeSlide('down'));
 const changeSlide = (direction) => {
     const sliderHeight = containerSlider.clientHeight;
     if(direction == 'up') {
-        activeSlidesLength++;
-        if(activeSlidesLength > slidesLength - 1) {
-            activeSlidesLength = 0;
+        activeSlidesIndex++;
+        if(activeSlidesIndex > slidesLength - 1) {
+           activeSlidesIndex = 0;
         }
     }
 
     else if(direction == 'down') {
-        activeSlidesLength--;
-        if(activeSlidesLength < 0) {
-             activeSlidesLength = slidesLength - 1;
+       activeSlidesIndex--;
+        if(activeSlidesIndex < 0) {
+             activeSlidesIndex = slidesLength - 1;
         }
     }
 
-    
-
-
-    
+    slideRight.style.transform = `translateY(-${activeSlidesIndex * sliderHeight}px)`;
+    slideLeft.style.transform = `translateY(${activeSlidesIndex * sliderHeight}px)`;
 }
